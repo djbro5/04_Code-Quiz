0b525219d8d4c2b6bd17ffbe7610e134cc86378c
unknown <david.bn@bigpond.com>
2020-04-28 23:04:09 +0930

    finished app

---
+++ README.md
@@ -0,0 +1,73 @@
+
+## PROJECT TITLE:
+
+ Code Quiz
+
+## PROBLEM DESCRIPTION:
+
+Method to test many web developer's knowledge of coding.
+
+## SOLUTION:
+
+A code quiz with multiple-choice questions that can optionally store an individual's name with associated score.
+
+## USER STORY:
+
+AS a coding bootcamp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+so THAT I can gauge my progress compared to my peers
+
+## APPLICATION DESCRIPTION
+
+Web developer coding quiz that has instructions for use,  five multiple choise questions and a quiz countdown timer. Final score of participant is able to be locally stored. 
+
+### a) OPERATION
+
+1) Participant arrives at landing page and is presented with instructions for use and how quiz is scored.
+
+2) Upon click of start button participant is presented with five multiple choice questions.
+
+3) Each question has four possible answers from which participant must select correct answer within allocated 75 second countdown.
+
+4) Scoring:
+                 - Correct answer selected will result in green answer earning 10 points and gaining 10 seconds added to countdown time. 
+
+                 - If incorrect answer selected, answer will result in red answer and 15 seconds taken off countdown time.
+
+5) Quiz will stop when  countdown reaches 0 seconds.
+
+6) Participant can save their score with name and return to landing page or do quiz again.
+  
+### b) DEVELOPMENT
+
+Technologies used web application include:
+    * HTML
+    * CSS
+    * Flexbox / Bootstrap
+    * JavaScript
+
+## LICENSE
+
+MIT License
+
+Copyright (c) 2019 David Brown
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
---
+++ assets/js/quiz.js
@@ -0,0 +1,140 @@
+// Quiz
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+const timerElement = document.getElementById("timerCount");
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+var timerInterval;
+var timerCount = 60;
+
+let questions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript??",
+    choice1: "<script>",
+    choice2: "<javascript>",
+    choice3: "<js>",
+    choice4: "<scripting>",
+    answer: 1
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choice1: "<script href='xxx.js'>",
+    choice2: "<script name='xxx.js'>",
+    choice3: "<script src='xxx.js'>",
+    choice4: "<script file='xxx.js'>",
+    answer: 3
+  },
+  {
+    question: " How do you write 'Hello World' in an alert box?",
+    choice1: "msgBox('Hello World');",
+    choice2: "alertBox('Hello World');",
+    choice3: "msg('Hello World');",
+    choice4: "alert('Hello World');",
+    answer: 4
+  },
+
+  {
+    question: "Which of the following is not a logical operator?",
+    choice1: "&&",
+    choice2: "!",
+    choice3: "||",
+    choice4: "|",
+    answer: 4
+},
+
+{
+  question: "The condition in an if / else statement is enclosed within ____.",
+  choice1: "quotes", 
+  choice2: "curly brackets", 
+  choice3:"parentheses", 
+  choice4: "square brackets",
+  answer: 3
+},
+];
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 5;
+
+startQuiz = () => {
+  questionCounter = 0;
+  score = 0;
+  timerInterval = setInterval(timer, 1000);
+  availableQuesions = [...questions];
+  getNewQuestion();
+};
+
+timer = () => {
+  timerCount--;
+  if (timerCount === 0) {
+    clearInterval(timerInterval);
+    localStorage.setItem("mostRecentScore", score);
+    return window.location.assign("end.html");
+  } else {
+    timerElement.textContent = timerCount;
+  }
+};
+
+getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    clearInterval(timerInterval);
+    localStorage.setItem("mostRecentScore", score);
+    return window.location.assign("end.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      timerCount = timerCount + 10;
+      incrementScore(CORRECT_BONUS);
+    } else {
+      timerCount = timerCount - 10;
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startQuiz();
